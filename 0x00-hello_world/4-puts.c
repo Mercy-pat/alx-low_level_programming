4cc1567d43032181fdad9388ee5092c61de1755e
Mercy Ajiboso <mercytitilop01@gmail.com>
2022-09-08 12:23:13 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
-/**
- *  * main - main block
- *   * Return: 0
- *    */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
-[6:38 am, 08/09/2022] +260 96 2803778: task 5
-#include <stdio.h>
-/**
- *  * main - entry point
- *   * Return: 0
- *    */
-int main(void)
-{
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
-}
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
